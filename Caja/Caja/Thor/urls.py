106a12d3a590dd15b53f0064920652c6d23e3c31
Skr4ti <ariascastroluisesteban@gmail.com>
2026-04-18 02:43:03 -0600

    Delete Caja directory

--- Caja/Caja/Thor/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import caja, borrar_venta,editar_venta
-
-urlpatterns = [
-    path('', caja, name='caja'),
-    path('borrar/<int:id>/', borrar_venta, name='borrar'),
-    path('editar/<int:id>/', editar_venta, name='editar'),
-]
